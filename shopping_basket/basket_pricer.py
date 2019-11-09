8ee59f45ca6f3cf6a1d0108c33ed6efb289bc2bc
Frank Osayi <osayi@rapidratings.com>
2019-11-09 10:38:58 +0000

    adding functino for empty basket

--- shopping_basket/basket_pricer.py
+++ shopping_basket/basket_pricer.py
@@ -1,5 +1,8 @@
+
 def is_basket_empty(basket):
-    pass
+    if not basket:
+        return True
+    return False
 
 
 def is_catalogue_empty(catalogue):
@@ -13,11 +16,14 @@ def are_basket_items_in_catalogue(basket, catalogue):
 def get_sub_total(basket, catalogue):
     pass
 
+
 def get_total_price(sub_total, discount):
     pass
 
+
 def get_total_discount(basket, offers, catalogue):
     pass
+
 
 def get_basket_price(basket, catalogue, offers):
     pass
--- shopping_basket/shopping_basket_tests/test_basket_pricer.py
+++ shopping_basket/shopping_basket_tests/test_basket_pricer.py
@@ -5,8 +5,19 @@ import basket_pricer
 
 class TestShoppingBasketPrice(unittest.TestCase):
 
-    def test1(self):
+    def setUp(self):
         pass
 
-    def test2(self):
-        pass
+    def test_is_basket_empty_true(self):
+        basket = {}
+        response = basket_pricer.is_basket_empty(basket)
+        self.assertTrue(response)
+
+    def test_is_basket_empty_false(self):
+        basket = {
+            'Baked Beans': 2,
+            'Biscuits': 1,
+            'Sardines': 2
+        }
+        response = basket_pricer.is_basket_empty(basket)
+        self.assertFalse(response)
