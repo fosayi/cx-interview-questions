753d9891e6a6775d6071c0f29dab19e3252e3c80
Frank Osayi <osayi@rapidratings.com>
2019-11-09 10:10:24 +0000

    adding base files

---
+++ shopping_basket/basket_pricer.py
@@ -0,0 +1,24 @@
+def is_basket_empty(basket):
+    pass
+
+
+def is_catalogue_empty(catalogue):
+    pass
+
+
+def are_basket_items_in_catalogue(basket, catalogue):
+    pass
+
+
+def get_sub_total(basket, catalogue):
+    pass
+
+def get_total_price(sub_total, discount):
+    pass
+
+def get_total_discount(basket, offers, catalogue):
+    pass
+
+def get_basket_price(basket, catalogue, offers):
+    pass
+
---
+++ shopping_basket/shopping_basket_tests/test_basket_pricer.py
@@ -0,0 +1,12 @@
+import unittest
+
+import basket_pricer
+
+
+class TestShoppingBasketPrice(unittest.TestCase):
+
+    def test1(self):
+        pass
+
+    def test2(self):
+        pass
